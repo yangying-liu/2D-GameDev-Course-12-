a4fe8bfe2dd383c7625ae7e2b248ca13e8ea8df7
yangying-liu <liuyangying2@gmail.com>
2024-11-29 23:48:54 -0800

    L7 completed

---
+++ learn-2d-gamedev-godot-4-0.13.0-macos/M07.telling_a_story_workbook/lessons/expressions.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
+@onready var button_sophia: Button = %ButtonSophia
+@onready var button_pink: Button = %ButtonPink
+@onready var button_regular: Button = %ButtonRegular
+@onready var button_sad: Button = %ButtonSad
+@onready var button_happy: Button = %ButtonHappy
