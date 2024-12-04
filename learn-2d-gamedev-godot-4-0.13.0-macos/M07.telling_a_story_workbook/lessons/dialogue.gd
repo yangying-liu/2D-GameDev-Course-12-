f718a2032685e37faa598e048a35ef963022147a
yaliu10 <yaliu10@s.sfusd.edu>
2024-12-04 13:13:50 -0800

    L11 completed

--- learn-2d-gamedev-godot-4-0.13.0-macos/M07.telling_a_story_workbook/lessons/dialogue.gd
+++ learn-2d-gamedev-godot-4-0.13.0-macos/M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -64,7 +64,7 @@ func show_text() -> void:
 	audio_stream_player.play(sound_start_position)
 	# We stop the audio when the text finishes appearing.
 	tween.finished.connect(audio_stream_player.stop)
-
+	slide_in()
 
 func advance() -> void:
 	current_item_index += 1
@@ -74,3 +74,14 @@ func advance() -> void:
 		get_tree().quit()
 	else:
 		show_text()
+
+func slide_in() -> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUART)
+	tween.set_ease(Tween.EASE_OUT)
+	
+	body.position.x = 200.0
+	tween.tween_property(body, "position:x", 0.0, 0.3)
+	
+	body.modulate.a = 0.0
+	tween.parallel().tween_property(body, "modulate:a", 1.0, 0.2)
