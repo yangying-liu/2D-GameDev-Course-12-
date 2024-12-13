a3b62b10e671112ebd82de5969063edcc665b612
yangying-liu <liuyangying2@gmail.com>
2024-12-12 22:48:29 -0800

    L2 + P1 completed

---
+++ learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/lessons/runner.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+var max_speed := 600.0
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * max_speed
+	move_and_slide()
--- learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/practices/L2.P1.asteroid_field/moving_ship.gd
+++ learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/practices/L2.P1.asteroid_field/moving_ship.gd
@@ -8,10 +8,11 @@ var direction := Vector2(0, 0)
 
 func _physics_process(_delta: float) -> void:
 	# Set the direction from keyboard inputs here.
-	direction = direction
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down") # direction = direction
 	# Set the velocity.
-	velocity = velocity
+	velocity = direction * max_speed # velocity = velocity
 	# Move the ship by calling the appropriate function from the CharacterBody2D node.
+	move_and_slide() #
 	
 	if velocity.length() > 0.0:
 		rotation = velocity.angle()
