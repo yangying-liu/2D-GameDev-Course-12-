fee14fa211ce3f553e6303905c3e8ad9d48be095
yangying-liu <liuyangying2@gmail.com>
2024-12-14 15:42:55 -0800

    L3 completed

--- learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/lessons/runner.gd
+++ learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/lessons/runner.gd
@@ -2,7 +2,38 @@ extends CharacterBody2D
 
 var max_speed := 600.0
 
-func _physics_process(delta: float) -> void:
+@onready var _skin: Sprite2D = $_Skin
+
+
+const RUNNER_DOWN = preload("res://assets/runner_down.png")
+const RUNNER_DOWN_RIGHT = preload("res://assets/runner_down_right.png")
+const RUNNER_RIGHT = preload("res://assets/runner_right.png")
+const RUNNER_UP = preload("res://assets/runner_up.png")
+const RUNNER_UP_RIGHT = preload("res://assets/runner_up_right.png")
+
+const UP_RIGHT = Vector2.UP + Vector2.RIGHT
+const UP_LEFT = Vector2.UP + Vector2.LEFT
+const DOWN_RIGHT = Vector2.DOWN + Vector2.RIGHT
+const DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
+
+
+func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * max_speed
 	move_and_slide()
+
+	var direction_discrete := direction.sign()
+	match direction_discrete:
+		Vector2.RIGHT, Vector2.LEFT:
+			_skin.texture = RUNNER_RIGHT
+		Vector2.UP:
+			_skin.texture = RUNNER_UP
+		Vector2.DOWN:
+			_skin.texture = RUNNER_DOWN
+		UP_RIGHT, UP_LEFT:
+			_skin.texture = RUNNER_UP_RIGHT
+		DOWN_RIGHT, DOWN_LEFT:
+			_skin.texture = RUNNER_DOWN_RIGHT
+
+	if direction_discrete.length() > 0:
+		_skin.flip_h = direction.x < 0.0
