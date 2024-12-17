a1c3eb9adbe0afb0ba88e2c131cb34ec2a1151fd
yangying-liu <liuyangying2@gmail.com>
2024-12-17 10:38:15 -0800

    L5 + P1 completed

--- learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/lessons/runner.gd
+++ learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/lessons/runner.gd
@@ -3,37 +3,26 @@ extends CharacterBody2D
 @export var max_speed := 600.0
 @export var acceleration := 1200.0
 @export var deceleration := 1080.0
-@onready var _skin: Sprite2D = $_Skin
 
-const RUNNER_DOWN = preload("res://assets/runner_down.png")
-const RUNNER_DOWN_RIGHT = preload("res://assets/runner_down_right.png")
-const RUNNER_RIGHT = preload("res://assets/runner_right.png")
-const RUNNER_UP = preload("res://assets/runner_up.png")
-const RUNNER_UP_RIGHT = preload("res://assets/runner_up_right.png")
+@onready var _runner_visual: RunnerVisual = %RunnerVisualRed
 
-const UP_RIGHT = Vector2.UP + Vector2.RIGHT
-const UP_LEFT = Vector2.UP + Vector2.LEFT
-const DOWN_RIGHT = Vector2.DOWN + Vector2.RIGHT
-const DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
-
-
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * max_speed
+	var has_input_direction := direction.length() > 0.0
+	if has_input_direction:
+		var desired_velocity := direction * max_speed
+		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 	move_and_slide()
 
-	var direction_discrete := direction.sign()
-	match direction_discrete:
-		Vector2.RIGHT, Vector2.LEFT:
-			_skin.texture = RUNNER_RIGHT
-		Vector2.UP:
-			_skin.texture = RUNNER_UP
-		Vector2.DOWN:
-			_skin.texture = RUNNER_DOWN
-		UP_RIGHT, UP_LEFT:
-			_skin.texture = RUNNER_UP_RIGHT
-		DOWN_RIGHT, DOWN_LEFT:
-			_skin.texture = RUNNER_DOWN_RIGHT
-
-	if direction_discrete.length() > 0:
-		_skin.flip_h = direction.x < 0.0
+	if direction.length() > 0.0:
+		_runner_visual.angle = rotate_toward(_runner_visual.angle, direction.orthogonal().angle(), 8.0 * delta)
+		var current_speed_percent := velocity.length() / max_speed
+		_runner_visual.animation_name = (
+			RunnerVisual.Animations.WALK
+			if current_speed_percent < 0.8
+			else RunnerVisual.Animations.RUN
+		)
+	else:
+		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
--- learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/practices/L5.P1.smooth_game/smooth_ship.gd
+++ learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/practices/L5.P1.smooth_game/smooth_ship.gd
@@ -15,14 +15,14 @@ func _physics_process(delta: float) -> void:
 	if is_moving:
 		var desired_velocity := direction * max_speed
 		# make sure to make the velocity progress toward the desired state over time
-		velocity = desired_velocity
+		velocity = velocity.move_toward(desired_velocity, acceleration * delta) # velocity = desired_velocity
 	else:
 		# make sure the make the velocity process toward zero over time
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta) # velocity = Vector2.ZERO
 	
 	if direction.length() > 0:
 		# make sure to make the rotation progress towards the desired angle over time
 		# make sure to use `orthogonal()`!
 		# and don't forget to use `rotation_speed`
-		rotation = direction.angle()
+		rotation = rotate_toward(rotation, direction.orthogonal().angle(), rotation_speed * delta) # rotation = direction.angle()
 	move_and_slide()
