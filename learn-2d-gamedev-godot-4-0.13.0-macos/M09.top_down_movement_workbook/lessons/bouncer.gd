86a73f2d68e5a6b1ebc3ba2697f46beccf10804b
yaliu10 <yaliu10@s.sfusd.edu>
2024-12-19 11:22:26 -0800

    L13 completed

--- learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/lessons/bouncer.gd
+++ learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/lessons/bouncer.gd
@@ -6,10 +6,17 @@ extends CharacterBody2D
 
 @onready var dust: GPUParticles2D = %Dust
 @onready var runner_visual: RunnerVisual = %RunnerVisualPurple
+@onready var hit_box: Area2D = %HitBox
+
+func _ready() -> void:
+	hit_box.body_entered.connect(func(body: Node) -> void:
+		if body is Runner:
+			get_tree().call_deferred("reload_current_scene")
+	)
 
 func _physics_process(delta: float) -> void:
-	var direction := global_position.direction_to(get_global_mouse_position())
-	var distance := global_position.distance_to(get_global_mouse_position())
+	var direction := global_position.direction_to(get_global_player_position())
+	var distance := global_position.distance_to(get_global_player_position())
 	var speed := max_speed if distance > 100 else max_speed * distance / 100
 	var desired_velocity := direction * speed
 
@@ -29,3 +36,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		runner_visual.animation_name = RunnerVisual.Animations.IDLE
 		dust.emitting = false
+
+func get_global_player_position() -> Vector2:
+	return get_tree().root.get_node("Game/Runner").global_position
--- learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/lessons/game.gd
+++ learn-2d-gamedev-godot-4-0.13.0-macos/M09.top_down_movement_workbook/lessons/game.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var finish_line: FinishLine = %FinishLine
 @onready var count_down: CountDown = %CountDown
 @onready var runner: Runner = %Runner
+@onready var bouncer: CharacterBody2D = %Bouncer
 
 func _ready() -> void:
 	finish_line.body_entered.connect(func (body: Node) -> void:
@@ -31,4 +32,11 @@ func _ready() -> void:
 	count_down.counting_finished.connect(
 		func() -> void:
 			runner.set_physics_process(true)
+	)
+	
+	bouncer.set_physics_process(false)
+
+	count_down.counting_finished.connect(
+		func() -> void:
+			bouncer.set_physics_process(true)
 	)
